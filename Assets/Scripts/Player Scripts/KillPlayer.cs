f43d14b487ea314c90c2891141e27222a7812ff8
lujamitch <lujamitch@gmail.com>
2023-05-04 21:36:20 +0100

    fix

--- Assets/Scripts/Player Scripts/KillPlayer.cs
+++ Assets/Scripts/Player Scripts/KillPlayer.cs
@@ -6,16 +6,12 @@ using UnityEngine.SceneManagement;
 public class KillPlayer : MonoBehaviour
 {
     public GameOver GameOver;
-    private AudioSource sfx;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         //If collision detected is with player
         if (other.CompareTag("Player"))
         {
-            //Play death sound and display GameOver screen
-            sfx = GetComponent<AudioSource>();
-            sfx.Play();
             GameOver.Setup();
         }
     }
